1fef1fe6fd5e65841232972a1710388310795ffd
unknown <saschadiegel@gmx.de>
2018-11-03 13:59:46 +0100

    'first_commit'

---
+++ learning_users/basic_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import re_path
+from basic_app import views
+
+#template urls!
+app_name = 'basic_app'
+
+urlpatterns=[
+    re_path(r'^register/$',views.register,name='register'),
+    re_path(r'^user_login/$',views.user_login,name='user_login')
+]
